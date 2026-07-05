8b7a502947e86d1060f15def61b9278d58a15494
pravinkumar <pravinshanmugam1995@gmail.com>
2026-07-05 18:08:41 +0530

    Add poultry-backend package with initial dependencies and main function

---
+++ poultry-backend/src/main.rs
@@ -0,0 +1,58 @@
+//! This is an example function that leverages the Lambda Rust runtime HTTP support
+//! and the [axum](https://docs.rs/axum/latest/axum/index.html) web framework.  The
+//! runtime HTTP support is backed by the [tower::Service](https://docs.rs/tower-service/0.3.2/tower_service/trait.Service.html)
+//! trait.  Axum's applications are also backed by the `tower::Service` trait.  That means
+//! that it is fairly easy to build an Axum application and pass the resulting `Service`
+//! implementation to the Lambda runtime to run as a Lambda function.  By using Axum instead
+//! of a basic `tower::Service` you get web framework niceties like routing, request component
+//! extraction, validation, etc.
+use axum::{
+    extract::{Path, Query},
+    http::StatusCode,
+    response::Json,
+    routing::{get, post},
+    Router,
+};
+use lambda_http::{run, tracing, Error};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use std::env::set_var;
+
+#[derive(Deserialize, Serialize)]
+struct Params {
+    first: Option<String>,
+    second: Option<String>,
+}
+
+async fn root() -> Json<Value> {
+    Json(json!({ "msg": "I am GET /" }))
+}
+
+/// Example on how to return status codes and data from an Axum function
+async fn health_check() -> (StatusCode, String) {
+    let health = true;
+    match health {
+        true => (StatusCode::OK, "Healthy!".to_string()),
+        false => (StatusCode::INTERNAL_SERVER_ERROR, "Not healthy!".to_string()),
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // If you use API Gateway stages, the Rust Runtime will include the stage name
+    // as part of the path that your application receives.
+    // Setting the following environment variable, you can remove the stage from the path.
+    // This variable only applies to API Gateway stages,
+    // you can remove it if you don't use them.
+    // i.e with: `GET /test-stage/todo/id/123` without: `GET /todo/id/123`
+    //set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
+
+    // required to enable CloudWatch error logging by the runtime
+    tracing::init_default_subscriber();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/health/", get(health_check));
+
+    run(app).await
+}
